4fe0a8cebee4f4ab171f5502d7b27daa433209a2
Svitlana <svetlanayurevna98@gmail.com>
2025-10-26 22:11:19 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+const SITE_URL = 'https://08-zustand-iota-rouge.vercel.app';
+
+export const metadata: Metadata = {
+  title: 'NoteHub – simple notes manager',
+  description:
+    'NoteHub is a simple and efficient application for managing personal notes with search, tags and pagination.',
+  openGraph: {
+    title: 'NoteHub – simple notes manager',
+    description:
+      'Create, browse and manage your notes with search and tags in NoteHub.',
+    url: SITE_URL,
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub',
+      },
+    ],
+    type: 'website',
+  },
+};
+
+export default function RootLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <TanStackProvider>
+          <Header />
+          {children}
+          {modal}
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
